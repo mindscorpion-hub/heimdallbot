3647f546eadf239c3bc42a315c968629e516df9d
mindscorpion-hub <mindscorpion@proton.me>
2025-11-09 22:31:50 -0500

    initial commit: basic CLI prompts

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+def main():
+    print("Hello from heimdallbot!")
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    if len(sys.argv) <= 1:
+        print('Please input a prompt: uv run main.py <"your prompt here">')
+        sys.exit(1)
+    prompt = sys.argv[1]
+    
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+    response = client.models.generate_content(
+    model = 'gemini-2.0-flash-001', 
+    contents = messages
+    )
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+    main()

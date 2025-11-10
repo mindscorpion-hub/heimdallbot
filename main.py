5ccbd35a2b7056deae294b10f2a33b48a970495b
mindscorpion-hub <mindscorpion@proton.me>
2025-11-09 22:51:12 -0500

    added verbose capability

--- main.py
+++ main.py
@@ -11,11 +11,13 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    if len(sys.argv) <= 1:
+    if len(sys.argv) == 1:
         print('Please input a prompt: uv run main.py <"your prompt here">')
         sys.exit(1)
-    prompt = sys.argv[1]
     
+    if len(sys.argv) >= 2:
+        prompt = sys.argv[1]
+        
     messages = [
     types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
@@ -23,9 +25,12 @@ def main():
     model = 'gemini-2.0-flash-001', 
     contents = messages
     )
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if len(sys.argv) >= 3:
+    #    print(response.text)
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 if __name__ == "__main__":
     main()

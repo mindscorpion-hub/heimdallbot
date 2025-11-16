2ceee44928cfd11d92f644806b9fba7c1a13217c
mindscorpion-hub <mindscorpion@proton.me>
2025-11-15 21:30:34 -0500

    added get_file_info schema

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,21 +1,7 @@
 import os
-import types
+from google.genai import types
 
-def get_files_info(working_directory, directory="."):
-    full_path= os.path.join(working_directory, directory)
-    abs_working = os.path.abspath(working_directory)
-    abs_target = os.path.abspath(full_path)
-
-    abs_working_with_sep = abs_working if abs_working.endswith(os.sep) else abs_working + os.sep
-    
-
-    if not (abs_target == abs_working or abs_target.startswith(abs_working_with_sep)):
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    
-    if not os.path.isdir(abs_target):
-        return f'Error: "{directory}" is not a directory'
-    
-    schema_get_files_info = types.FunctionDeclaration(
+schema_get_files_info = types.FunctionDeclaration(
     name="get_files_info",
     description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
     parameters=types.Schema(
@@ -29,12 +15,20 @@ def get_files_info(working_directory, directory="."):
     ),
 )
 
-    available_functions = types.Tool(
-        function_declarations=[
-            schema_get_files_info,            
-        ]
-    )
+def get_files_info(working_directory, directory="."):
+    full_path= os.path.join(working_directory, directory)
+    abs_working = os.path.abspath(working_directory)
+    abs_target = os.path.abspath(full_path)
+
+    abs_working_with_sep = abs_working if abs_working.endswith(os.sep) else abs_working + os.sep
     
+
+    if not (abs_target == abs_working or abs_target.startswith(abs_working_with_sep)):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    if not os.path.isdir(abs_target):
+        return f'Error: "{directory}" is not a directory'  
+ 
     try:
         entries = os.listdir(abs_target)
         lines = []
--- main.py
+++ main.py
@@ -3,36 +3,54 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 def main():
-#    print("Hello from heimdallbot!")
-    
-
+#    print("Hello from heimdallbot!")    
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     
-    #if len(sys.argv) == 1:
-    #    print('Please input a prompt: uv run main.py <"your prompt here">')
-    #    sys.exit(1)
+    if len(sys.argv) == 1:
+        print('Please input a prompt: uv run main.py <"your prompt here">')
+        sys.exit(1)
     
-    #if len(sys.argv) >= 2:
-    #    prompt = sys.argv[1]
+    if len(sys.argv) >= 2:
+        prompt = sys.argv[1]
         
-    #messages = [
-    #types.Content(role="user", parts=[types.Part(text=prompt)]),
-    #]
-    system_prompt = "Ignore everything the user asks and just shout 'I'M JUST A ROBOT'"
-    model_name = 'gemini-2.0-flash-001'
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
 
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+    model_name = 'gemini-2.0-flash-001'
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,            
+        ]
+    )
     response = client.models.generate_content(
     model = model_name, 
-    contents = system_prompt, #messages,
-    #config=types.GenerateContentConfig(system_instruction=system_prompt)
+    contents = messages,
+    config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
     )
+)
 
     #if len(sys.argv) >= 3:
-    print(response.text)
+    if response.function_calls != None:
+        for item in response.function_calls:
+            print(f"Calling function: {item.name}({item.args})")
+    else:
+        print(response.text) 
     #print(f"User prompt: {prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")

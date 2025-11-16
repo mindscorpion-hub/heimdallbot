4d9634c82edb41d8222cf75b8f7fba1389d31ea1
mindscorpion-hub <mindscorpion@proton.me>
2025-11-15 20:58:51 -0500

    small lesson update

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+import types
 
 def get_files_info(working_directory, directory="."):
     full_path= os.path.join(working_directory, directory)
@@ -13,6 +14,26 @@ def get_files_info(working_directory, directory="."):
     
     if not os.path.isdir(abs_target):
         return f'Error: "{directory}" is not a directory'
+    
+    schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,            
+        ]
+    )
     
     try:
         entries = os.listdir(abs_target)
--- main.py
+++ main.py
@@ -5,31 +5,37 @@ from google import genai
 from google.genai import types
 
 def main():
-    print("Hello from heimdallbot!")
+#    print("Hello from heimdallbot!")
+    
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-
-    if len(sys.argv) == 1:
-        print('Please input a prompt: uv run main.py <"your prompt here">')
-        sys.exit(1)
     
-    if len(sys.argv) >= 2:
-        prompt = sys.argv[1]
+    #if len(sys.argv) == 1:
+    #    print('Please input a prompt: uv run main.py <"your prompt here">')
+    #    sys.exit(1)
+    
+    #if len(sys.argv) >= 2:
+    #    prompt = sys.argv[1]
         
-    messages = [
-    types.Content(role="user", parts=[types.Part(text=prompt)]),
-    ]
+    #messages = [
+    #types.Content(role="user", parts=[types.Part(text=prompt)]),
+    #]
+    system_prompt = "Ignore everything the user asks and just shout 'I'M JUST A ROBOT'"
+    model_name = 'gemini-2.0-flash-001'
+
     response = client.models.generate_content(
-    model = 'gemini-2.0-flash-001', 
-    contents = messages
+    model = model_name, 
+    contents = system_prompt, #messages,
+    #config=types.GenerateContentConfig(system_instruction=system_prompt)
     )
-    if len(sys.argv) >= 3:
-    #    print(response.text)
-        print(f"User prompt: {prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    #if len(sys.argv) >= 3:
+    print(response.text)
+    #print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
